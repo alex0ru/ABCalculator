0635d44f395daf26e314df02f6ce12980d8ea0d8
alex0ru <alex0ru@gmail.com>
2023-07-29 18:09:02 +0300

    Создание главного окна программы

---
+++ ABCalculator.py
@@ -0,0 +1,16 @@
+# А/В Калькулятор
+
+import tkinter as tk
+
+
+# Создание главного окна программы
+wMain = tk.Tk()
+wMain.geometry('280x300')
+wMain.title('А/В Калькулятор')
+
+#-----
+
+
+
+# Запуск цикла mainloop
+wMain.mainloop()
